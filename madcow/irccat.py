8733bb3bea228b4cd1558cc6c96a9ff725cb769d
Luke GB <git@lukegb.com>
2014-01-30 13:49:15 +0000

    Add colour support

--- madcow/irccat.py
+++ madcow/irccat.py
@@ -2,6 +2,47 @@ from SocketServer import TCPServer, ThreadingMixIn, StreamRequestHandler
 
 from .util import Request
 
+class IRCColours(object):
+    COLOUR_PREFIX = "\x03"
+
+    NORMAL    = "\x0f"
+    BOLD      = "\x02"
+    UNDERLINE = "\x1f"
+    REVERSE   = "\x16"
+
+    WHITE      = COLOUR_PREFIX + "00"
+    BLACK      = COLOUR_PREFIX + "01"
+    DARK_BLUE  = COLOUR_PREFIX + "02"
+    DARK_GREEN = COLOUR_PREFIX + "03"
+    RED        = COLOUR_PREFIX + "04"
+    BROWN      = COLOUR_PREFIX + "05"
+    PURPLE     = COLOUR_PREFIX + "06"
+    OLIVE      = COLOUR_PREFIX + "07"
+    YELLOW     = COLOUR_PREFIX + "08"
+    GREEN      = COLOUR_PREFIX + "09"
+    TEAL       = COLOUR_PREFIX + "10"
+    CYAN       = COLOUR_PREFIX + "11"
+    BLUE       = COLOUR_PREFIX + "12"
+    MAGENTA    = COLOUR_PREFIX + "13"
+    DARK_GRAY  = COLOUR_PREFIX + "14"
+    LIGHT_GRAY = COLOUR_PREFIX + "15"
+
+    export = [
+        'NORMAL', 'BOLD', 'UNDERLINE', 'REVERSE',
+        'WHITE', 'BLACK', 'DARK_BLUE', 'DARK_GREEN',
+        'RED', 'BROWN', 'PURPLE', 'OLIVE', 'YELLOW',
+        'GREEN', 'TEAL', 'CYAN', 'BLUE', 'MAGENTA',
+        'DARK_GRAY', 'LIGHT_GRAY'
+    ]
+    prefix_chars = '%#'
+
+    @classmethod
+    def colourise(cls, line):
+        for prefix_char in cls.prefix_chars:
+            for colour in cls.export:
+                line = line.replace(prefix_char + colour, getattr(cls, colour))
+        return line
+
 class IRCCatRequestHandler(StreamRequestHandler):
     def __init__(self, *args, **kwargs):
         StreamRequestHandler.__init__(self, *args, **kwargs)
@@ -21,6 +62,8 @@ class IRCCatRequestHandler(StreamRequestHandler):
         
         recipients_str, _, line = line.partition(' ')
         base_recipients = recipients_str.split(',')
+
+        line = IRCColours.colourise(line)
 
         recipients = set()
         for recipient in base_recipients:
